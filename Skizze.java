36b3cc06fe441fc9a07b8ed47ac5338521fdb41b
Sabine <10sloe@itg-dah.info>
2018-10-03 11:19:20 +0200

    Kommentare hinzugefügt

--- Skizze.java
+++ Skizze.java
@@ -3,14 +3,15 @@ import javafx.scene.paint.Color;
 
 public class Skizze
 {
-    double x = 100;
+    double x = 100;   // x-Koordinate des Balls        
  
     // Diese Methode wird immer und immer wieder ausgeführt
     void draw(GraphicsContext gc){
-        gc.clearRect(0,0,400,400);
-        gc.setFill(Color.RED);
-        gc.fillOval(x,100,20,20);  
-        x = x + 1;
+        gc.clearRect(0,0,400,400);  // Löschen der Zeichenfläche
+        gc.setFill(Color.RED);      // Setzen der Füllfarbe
+          
+        x = x + 1;               
+        gc.fillOval(x,100,20,20);   // Zeichnen des Balls
     }
 
 }
